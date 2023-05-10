6c8eb7cee0beb96580cf514e647363416204ae39
kunal <kunalch36@gmail.com>
2023-05-10 11:56:29 +0530

    - Using locale in SimpleDateFormat

--- app/src/main/java/com/kc/marvelapp/util/Utils.kt
+++ app/src/main/java/com/kc/marvelapp/util/Utils.kt
@@ -1,16 +1,18 @@
 package com.kc.marvelapp.util
 
+import android.util.Log
 import java.text.ParseException
 import java.text.SimpleDateFormat
+import java.util.Locale
 
 object Utils {
     fun getDate(input: String): String{
         var result = ""
         try {
-            val inputDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+            val inputDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault())
             val date = inputDateFormat.parse(input)
-            val reqDateFormat = SimpleDateFormat("MMMM dd, yyyy")
-            result = reqDateFormat.format(date)
+            val reqDateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
+            result = date?.let { reqDateFormat.format(it) } ?: run { "" }
         } catch (e: ParseException){
             e.printStackTrace()
         }
