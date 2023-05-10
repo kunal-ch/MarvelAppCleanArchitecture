72f5b1275a8d28bdb40fe48f79596f0d25cf95ec
kunal <kunalch36@gmail.com>
2023-05-10 12:09:28 +0530

    - Test case: Utils

---
+++ app/src/test/java/com/kc/marvelapp/util/UtilsTest.kt
@@ -0,0 +1,29 @@
+package com.kc.marvelapp.util
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class UtilsTest {
+    @Test
+    fun getDateValid(){
+        val input = "2013-10-24T14:32:08-0400"
+        val output = "October 25, 2013"
+        val result = Utils.getDate(input)
+        assertThat(result).isEqualTo(output)
+    }
+
+    @Test
+    fun getDateInvalid(){
+        val input = "2014-10-24T14:32:08-0400"
+        val output = "October 25, 2013"
+        val result = Utils.getDate(input)
+        assertThat(result).isNotEqualTo(output)
+    }
+
+    @Test
+    fun getDateEmpty(){
+        val input = ""
+        val result = Utils.getDate(input)
+        assertThat(result).isEmpty()
+    }
+}
