f01c2be2d49917160c6e2b0a05786838f8c30a13
kunal <kunalch36@gmail.com>
2023-05-10 11:57:17 +0530

    - Dispatcher as dependency and success logic

--- app/src/main/java/com/kc/marvelapp/data/repository/MarvelRepositoryImpl.kt
+++ app/src/main/java/com/kc/marvelapp/data/repository/MarvelRepositoryImpl.kt
@@ -2,10 +2,11 @@ package com.kc.marvelapp.data.repository
 
 import com.kc.marvelapp.data.mapper.toAllCharactersResponse
 import com.kc.marvelapp.data.service.MarvelApi
+import com.kc.marvelapp.di.IoDispatcher
 import com.kc.marvelapp.domain.models.ComicCharacter
 import com.kc.marvelapp.domain.repository.MarvelRepository
 import com.kc.marvelapp.util.Resource
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
@@ -17,81 +18,57 @@ import javax.inject.Inject
  * Repository Implementation
  */
 class MarvelRepositoryImpl @Inject constructor(
-    private val api: MarvelApi
+    private val api: MarvelApi,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher,
     ): MarvelRepository {
 
     override suspend fun getCharacterListings(
     ): Flow<Resource<List<ComicCharacter>>> {
         return flow<Resource<List<ComicCharacter>>> {
             emit(Resource.Loading(true))
-
-            val remoteListings = try {
-                val response = api.getAllCharacters()
-                if (response.isSuccessful) {
-                    response.body()?.let { resultResponse ->
-                        resultResponse.toAllCharactersResponse().data.characters
-                    }
-                } else {
-                    listOf()
-                }
-            }  catch (e: HttpException) {
-                e.printStackTrace()
-                emit(Resource.Error("Couldn't load data"))
-                null
-            }
-            catch (e: IOException) {
-                e.printStackTrace()
-                emit(Resource.Error("Couldn't load data"))
-                null
-            }
-
+            val remoteListings = getList(api)
             remoteListings?.let { listings ->
-                emit(
-                    Resource.Success(
-                        data = listings
-                    )
-                )
+                emit(Resource.Success(data = listings))
                 emit(Resource.Loading(false))
+            } ?: run {
+                emit(Resource.Error("Couldn't load data"))
             }
-        }.flowOn(Dispatchers.IO)
+        }.flowOn(dispatcher)
     }
 
     override suspend fun getCharacterInfo(
         id: String,
     ): Flow<Resource<ComicCharacter>> {
-        return flow {
+        return flow<Resource<ComicCharacter>> {
             emit(Resource.Loading(true))
-
-            val remoteListings = try {
-                val response = api.getCharacter(id)
-                if (response.isSuccessful) {
-                    response.body()?.let { resultResponse ->
-                        resultResponse.toAllCharactersResponse().data.characters
-                    }
-                } else {
-                    listOf()
-                }
-            } catch (e: HttpException) {
-                e.printStackTrace()
-                emit(Resource.Error("Couldn't load data"))
-                null
-            }
-            catch (e: IOException) {
-                e.printStackTrace()
-                emit(Resource.Error("Couldn't load data"))
-                null
-            }
+            val remoteListings = getList(api)
             remoteListings?.let { remoteListings
                 if (remoteListings.isNotEmpty()) {
-                    emit(
-                        Resource.Success(
-                            data = remoteListings[0]
-                        )
-                    )
+                    emit(Resource.Success(data = remoteListings[0]))
                 }
+            } ?: run {
+                emit(Resource.Error("Couldn't load data"))
             }
             emit(Resource.Loading(false))
-            return@flow
-        }.flowOn(Dispatchers.IO)
+        }.flowOn(dispatcher)
+    }
+
+    private suspend fun getList(api : MarvelApi) : List<ComicCharacter>? {
+        val remoteListings = try {
+            val response = api.getAllCharacters()
+            if (response.isSuccessful) {
+                response.body()?.toAllCharactersResponse()?.data?.characters
+            } else {
+                listOf()
+            }
+        }  catch (e: HttpException) {
+            e.printStackTrace()
+            null
+        }
+        catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
+        return remoteListings
     }
 }
