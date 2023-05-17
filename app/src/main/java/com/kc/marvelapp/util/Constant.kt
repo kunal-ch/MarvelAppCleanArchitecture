5bbe36ff7d2f000798813471543c93f0c7994e8e
kunal <kunalch36@gmail.com>
2023-05-17 17:39:11 +0530

    - Generic getDate method

--- app/src/main/java/com/kc/marvelapp/util/Constant.kt
+++ app/src/main/java/com/kc/marvelapp/util/Constant.kt
@@ -3,5 +3,7 @@ package com.kc.marvelapp.util
 class Constant {
     companion object {
         const val BASE_URL = "https://gateway.marvel.com"
+        const val UTC_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ"
+        const val CHAR_DATE_FORMAT = "MMMM dd, yyyy"
     }
 }
--- app/src/main/java/com/kc/marvelapp/util/Utils.kt
+++ app/src/main/java/com/kc/marvelapp/util/Utils.kt
@@ -1,17 +1,16 @@
 package com.kc.marvelapp.util
 
-import android.util.Log
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 object Utils {
-    fun getDate(input: String): String{
+    fun getDate(input: String, inDateFormat: String = Constant.UTC_DATE_FORMAT, outDateFormat: String = Constant.CHAR_DATE_FORMAT): String{
         var result = ""
         try {
-            val inputDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault())
+            val inputDateFormat = SimpleDateFormat(inDateFormat, Locale.getDefault())
             val date = inputDateFormat.parse(input)
-            val reqDateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
+            val reqDateFormat = SimpleDateFormat(outDateFormat, Locale.getDefault())
             result = date?.let { reqDateFormat.format(it) } ?: run { "" }
         } catch (e: ParseException){
             e.printStackTrace()
