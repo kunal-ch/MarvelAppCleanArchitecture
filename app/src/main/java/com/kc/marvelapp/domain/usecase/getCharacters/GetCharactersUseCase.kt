6af3d769522e05a2c619e0d94e0e50d3a27d5fa4
kunal <kunalch36@gmail.com>
2023-05-18 15:37:04 +0530

    - Optimize imports

--- app/src/main/java/com/kc/marvelapp/domain/usecase/getCharacters/GetCharactersUseCase.kt
+++ app/src/main/java/com/kc/marvelapp/domain/usecase/getCharacters/GetCharactersUseCase.kt
@@ -1,9 +1,6 @@
 package com.kc.marvelapp.domain.usecase.getCharacters
 
-import com.kc.marvelapp.domain.models.ComicCharacter
 import com.kc.marvelapp.domain.repository.MarvelRepository
-import com.kc.marvelapp.util.Resource
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetCharactersUseCase @Inject constructor(
